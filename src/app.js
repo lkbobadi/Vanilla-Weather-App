b706bc0299a71e6abacc7064cc5256383e79c780
lkbobadi <lkbobadi@gmail.com>
2021-12-24 22:42:20 +0900

    daily forecast
    added and solved

--- src/app.js
+++ src/app.js
@@ -33,7 +33,6 @@ weekDay.innerHTML = days[day];
 date.innerHTML = time;
 
 function showTemperature(response) {
-  console.log(response.data);
   let icon = document.querySelector("#icon");
   icon.setAttribute(
     "src",
@@ -57,12 +56,23 @@ function showTemperature(response) {
   temperature = Math.round(response.data.list[0].main.temp);
   let mainTemp = document.querySelector(".main-temp");
   mainTemp.innerHTML = `${temperature}°F`;
+
+  let location = response.data.list[0].coord;
+  console.log(location);
+  getForecast(location);
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let units = "imperial";
+  let apiKey = `57076c7f47016215ae76e0f26aed1cee`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&units=${units}&appid=${apiKey}`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function handleSubmit(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#searchbar");
-  console.log(cityInput.value);
   searchCity(cityInput.value);
 }
 
@@ -72,7 +82,6 @@ function searchCity(city) {
   let units = "imperial";
   let apiKey = "57076c7f47016215ae76e0f26aed1cee";
   let apiUrl = `https://api.openweathermap.org/data/2.5/find?q=${city}&units=${units}&appid=${apiKey}`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(showTemperature);
 }
 
@@ -104,23 +113,37 @@ celsius.addEventListener("click", displayCelsius);
 let farenheit = document.querySelector(".farenheit");
 farenheit.addEventListener("click", displayFarenheit);
 
-function displayForecast() {
-  let forecast = document.querySelector("#forecast");
-  let forecastHTML = ` <h2 class="forecast-title">Forecast</h2><div class="forecast row mb-4 ms-1 me-1">`;
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu"];
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class = "forecast-list col-2"><div class="extra-days">${day}</div>
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastTitle = document.querySelector("#forecast");
+  let forecastHTML = ` <h2 class="forecast-title">Forecast</h2><div class="forecast row mb-4 ms-1 me-1">`;
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `<div class = "forecast-list col-2"><div class="extra-days">${formatDay(
+          forecastDay.dt
+        )}</div>
 <div class="forecast-symbol"><img src="https://openweathermap.org/img/wn/04n@2x.png" width="50px" alt=""></div>
-<div class="min-max row"><div class="forecast-max col-6">15F</div>
-<div class="forecast-min col-6">10F</div>
+<div class="min-max row"><div class="forecast-max col-6">${Math.round(
+          forecastDay.temp.max
+        )}°</div>
+<div class="forecast-min col-6">${Math.round(forecastDay.temp.min)}°</div>
 </div></div>`;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
-  forecast.innerHTML = forecastHTML;
-  console.log(forecastHTML);
+  forecastTitle.innerHTML = forecastHTML;
 }
+
 searchCity("Raleigh");
-displayForecast();
